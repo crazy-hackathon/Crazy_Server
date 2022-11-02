283af803162c8bcd52c278d24d7844cda241af5d
yongbin <rhdydqls1024@naver.com>
2022-11-03 08:10:54 +0900

    :seedling: :: initial commit

---
+++ src/main/java/com/project/crazy/CrazyApplication.java
@@ -0,0 +1,13 @@
+package com.project.crazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrazyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/crazy/CrazyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.crazy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrazyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
