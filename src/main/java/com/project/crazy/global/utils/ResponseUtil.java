87ffc84b4157f184ec730c57ba90ef0b107134d9
yongbin <rhdydqls1024@naver.com>
2022-11-04 02:18:18 +0900

    :memo: :: ResponseUtil 구현

---
+++ src/main/java/com/project/crazy/global/utils/ResponseUtil.java
@@ -0,0 +1,21 @@
+package com.project.crazy.global.utils;
+
+import com.project.crazy.domain.post.entity.Post;
+import com.project.crazy.domain.post.presentation.dto.response.PostResponse;
+
+import java.time.format.DateTimeFormatter;
+
+public class ResponseUtil {
+
+    public static PostResponse getPostResponse(Post post) {
+        return PostResponse.builder()
+                .postId(post.getPostId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .like(post.getLike())
+                .location(post.getLocation())
+                .createdAt(DateTimeFormatter.ofPattern("yyyy-MM-dd").format(post.getCreatedAt()))
+                .build();
+    }
+
+}
